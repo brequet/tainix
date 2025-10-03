d1251a3c5ab7b2b6637ee7a4b793503310dea405
brequet <requet.baptiste@gmail.com>
2025-10-03 22:19:05 +0200

    add aliases for commands

--- tainix-cli/src/cli.rs
+++ tainix-cli/src/cli.rs
@@ -11,18 +11,21 @@ pub struct Cli {
 #[derive(Subcommand, Debug)]
 pub enum Commands {
     /// Generates a new challenge folder from a Tainix challenge URL name
+    #[command(alias = "g")]
     Generate {
         /// The name of the challenge as it appears in the URL (e.g., "Utilisation-d-une-fonction")
         #[arg(value_name = "CHALLENGE_NAME")]
         name: String,
     },
     /// Tests a local challenge solution by running its TypeScript file
+    #[command(alias = "t")]
     Test {
         /// The code of the challenge to test (e.g., "BANK_1")
         #[arg(value_name = "CHALLENGE_CODE")]
         code: String,
     },
     /// Submits a challenge solution to the Tainix API
+    #[command(alias = "s")]
     Submit {
         /// The code of the challenge to submit (e.g., "BANK_1")
         #[arg(value_name = "CHALLENGE_CODE")]
